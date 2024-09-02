9263995dd8d4fc13d8dca7b606f197dbef97ce7e
zSnails <erizojuan33@gmail.com>
2024-09-01 23:53:45 -0600

    feat: render the board

--- main.go
+++ main.go
@@ -1,87 +1,89 @@
 package main
 
-import "fmt"
+import (
+	"image"
 
-type Side uint8
-
-const (
-	NONE Side = iota
-	WHITE
-	BLACK
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
 )
 
-type Piece interface {
-	IsValidMove(board *Board, x1, y1, x2, y2 int) bool
-	Side() Side
-}
+var blackKnight = NewKnight(BLACK)
+var blackPawn = NewPawn(BLACK)
+var blackQueen = NewQueen(BLACK)
+var blackKing = NewKing(BLACK)
+var blackBishop = NewBishop(BLACK)
+var blackRook = NewRook(BLACK)
 
-type Board [8][8]Piece
-
-func (b *Board) PieceAt(x, y int) Piece {
-	if x < 0 || y < 0 || x > 7 || y > 7 {
-		panic("unreachable: invalid position")
-	}
-	return b[x][y]
-}
-
-func (c *Board) CanMove(x1, y1, x2, y2 int) bool {
-	if x2 > 7 || y2 > 7 || x1 < 0 || x2 < 0 { // out of bounds check
-		return false
-	}
-
-	piece := c.PieceAt(x1, y1)
-	return piece != nil && piece.IsValidMove(c, x1, y1, x2, y2)
-}
-
-func (c *Board) Move(x1, y1, x2, y2 int) bool {
-	if c.CanMove(x1, y1, x2, y2) {
-		c[x2][y2] = c[x1][y1]
-		c[x1][y1] = nil
-		return true
-	}
-
-	return false
-}
-
-var blackRook = Rook{
-	side: BLACK,
-}
-
-var whiteRook = Rook{
-	side: WHITE,
-}
-
-var pawn = Pawn{
-	side: BLACK,
-}
-
-var knight = Knight{
-	side: WHITE,
-}
-
-var bishop = Bishop{
-	side: WHITE,
-}
-
-var king = King{
-	side: WHITE,
-}
-
-var queen = Queen{
-	side: WHITE,
-}
+var whiteKnight = NewKnight(WHITE)
+var whitePawn = NewPawn(WHITE)
+var whiteQueen = NewQueen(WHITE)
+var whiteKing = NewKing(WHITE)
+var whiteBishop = NewBishop(WHITE)
+var whiteRook = NewRook(WHITE)
 
 var board = Board{
-	{&knight, &pawn, nil, nil, nil, nil, nil, nil},
-	{&pawn, &pawn, &whiteRook, nil, nil, nil, nil, nil},
-	{&bishop, nil, nil, nil, nil, nil, nil, nil},
-	{nil, &pawn, nil, nil, nil, nil, nil, nil},
-	{&queen, nil, nil, nil, nil, nil, &pawn, nil},
-	{nil, nil, nil, nil, nil, nil, nil, nil},
-	{nil, nil, nil, nil, nil, nil, nil, nil},
-	{nil, nil, nil, nil, nil, nil, nil, nil},
+	{&whiteRook, &whitePawn, nil, nil, nil, nil, &blackPawn, &blackRook},
+	{&whiteKnight, &whitePawn, nil, nil, nil, nil, &blackPawn, &blackKnight},
+	{&whiteBishop, &whitePawn, nil, nil, nil, nil, &blackPawn, &blackBishop},
+	{&whiteKing, &whitePawn, nil, nil, nil, nil, &blackPawn, &blackQueen},
+	{&whiteQueen, &whitePawn, nil, nil, nil, nil, &blackPawn, &blackKing},
+	{&whiteBishop, &whitePawn, nil, nil, nil, nil, &blackPawn, &blackBishop},
+	{&whiteKnight, &whitePawn, nil, nil, nil, nil, &blackPawn, &blackKnight},
+	{&whiteRook, &whitePawn, nil, nil, nil, nil, &blackPawn, &blackRook},
+}
+
+type game struct {
+	board       *ebiten.Image
+	selectStart image.Point
+}
+
+// Draw implements ebiten.Game.
+func (g *game) Draw(screen *ebiten.Image) {
+	opts := ebiten.DrawImageOptions{}
+	screen.DrawImage(g.board, &opts)
+
+	for x, row := range board {
+		for y, piece := range row {
+			if piece != nil {
+				opts.GeoM.Translate(float64(16+(x*32)), float64((y*32)+16))
+				screen.DrawImage(piece.Sprite(), &opts)
+				opts.GeoM.Reset()
+			}
+		}
+	}
+}
+
+// Layout implements ebiten.Game.
+func (g *game) Layout(outsideWidth int, outsideHeight int) (screenWidth int, screenHeight int) {
+	// return 640, 480
+	return 288, 288
+}
+
+// Update implements ebiten.Game.
+func (g *game) Update() error {
+	if inpututil.IsMouseButtonJustPressed(ebiten.MouseButtonLeft) {
+		x, y := ebiten.CursorPosition()
+		cellX := (x - 16) / 32
+		cellY := (y - 16) / 32
+		g.selectStart = image.Pt(cellX, cellY)
+	} else if inpututil.IsMouseButtonJustReleased(ebiten.MouseButtonLeft) {
+		x, y := ebiten.CursorPosition()
+		cellX := (x - 16) / 32
+		cellY := (y - 16) / 32
+		board.Move(g.selectStart.X, g.selectStart.Y, cellX, cellY)
+	}
+	return nil
 }
 
 func main() {
-	fmt.Printf("board.Move(0, 0, 7, 7): %v\n", board.Move(4, 0, 2, 2))
+	boardImage, _, err := ebitenutil.NewImageFromFile("./assets/Classic/Board/Board - classic 1.png")
+	if err != nil {
+		panic(err)
+	}
+	if err := ebiten.RunGame(&game{
+		board: boardImage,
+	}); err != nil {
+		panic(err)
+	}
 }
