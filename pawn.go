5647c72ab3190f95e8ed63ce55059beb8abfda11
zSnails <erizojuan33@gmail.com>
2024-09-01 18:32:37 -0600

    fix: take which side the pawn belongs to into account

--- pawn.go
+++ pawn.go
@@ -22,12 +22,18 @@ func (p *Pawn) IsValidMove(board *Board, x1 int, y1 int, x2 int, y2 int) bool {
 	positiveSlope := y2-y1 == x2-x1
 	negativeSlope := y2-y1 == x1-x2
 
+	if p.Side() == White && (y2-y1) != 1 {
+		return false
+	} else if p.Side() == Black && (y2-y1) != -1 {
+		return false
+	}
+
 	other := board.PieceAt(x2, y2)
 	if positiveSlope || negativeSlope {
 		return p.CanTake(other)
 	}
 
-	return other == nil && x1 == x2 && (y2-y1) == 1
+	return other == nil && x1 == x2
 }
 
 var _ Piece = &Pawn{}
